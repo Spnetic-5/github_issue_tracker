a5e3b7ba95dd11f371c1ec9d085b5c37f60194fe
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-03-27 18:42:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,73 @@
-const dotenv = require('dotenv').config()
+require("dotenv").config();
 
-const discord = require('discord.js')
+const Discord = require("discord.js");
+const fs = require("fs");
 
-const client = new discord.Client()
+// Express Server
+const express = require("express");
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-client.on('ready', function() {
-    console.log('The client is running...');
+
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const prefix = process.env.PREFIX;
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+let channel;
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on('ready', async () => {
+  channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_NUMBER);
+  console.log("Bot is ready");
 })
 
-client.on('message', function(message) {
-    console.log(message)
+client.on('message', (msg) => {
+  // ignore if a bot sends the message
+  if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+
+  args = msg.content.slice(prefix.length).trim().split(/ +/);
+  command = args.shift().toLowerCase();
+
+  if (!client.commands.get(command)) {
+    return msg.reply("No such command found");
+  }
+
+  command = client.commands.get(command);
+
+  try {
+    command.execute(msg, args);
+  } catch (error) {
+    console.log(error);
+    return msg.channel.send("There was an error executing the command");
+  }
 })
 
-client.on('message', function(message) {
+client.on("githubMessage", (body) => {
+  const action = body.action;
+  const issueUrl = body.issue.html_url;
+  if (action === "opened") {
+    const message = `New issue created : ${issueUrl}`;
+    channel.send(message);
+  }
+})
 
-    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+app.post("/github", (req, res) => {
+  client.emit("githubMessage", req.body);
+  return res.status(200).json({ success: true });
+})
 
-    args = msg.content.slice(prefix.length).trim().split(/ +/);
-    command = arg.shift().toLowerCase();
-
-    console.log(args, command);
-    // if (message.author.bot) return;
-    // message.reply(`Hey! How u doin!, ${msg.author.username}`)
-});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log("Server started on port : ", port);
+})
